884b8e20243216c422f7dc1a0964fb893430fbf3
Marko Juvonen <marko.tt.juvonen@gmail.com>
2019-11-21 11:35:01 +0200

    käyttäjän lisäys ja koordinaattien lisäys tietokantaan annetun sijainnin perusteella

---
+++ Controllers/TiliController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TapahtumaLib;
+using TapahtumaLib.Models;
+
+namespace TapahtumaRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TiliController : ControllerBase
+    {
+        Kayttaja k = new Kayttaja();
+        // GET: api/Tili
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET: api/Tili/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST: api/Tili
+        [HttpPost]
+        public void Post([FromBody] Kayttajat kayttaja)
+        {
+            using (EventDBContext db = new EventDBContext())
+            {
+                db.Kayttajat.Add(kayttaja);
+                db.SaveChanges();
+            }
+        }
+
+        // PUT: api/Tili/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
